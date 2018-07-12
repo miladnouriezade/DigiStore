fb38559328773e5ada2f8bda45986e9060d03f20
Milad Nourizade <miladnouriezade@outlook.com>
2018-07-12 21:22:26 +0430

    Add assets .Use custom cells for search results

---
+++ DigiStore/View/SearchResultCell.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResultCell.swift
+//  DigiStore
+//
+//  Created by Milad Nourizade on 7/12/18.
+//  Copyright © 2018 Milad Nourizade. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    
+
+    
+}
