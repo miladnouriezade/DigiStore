ed4e3d4c46be0fa6f9589b97bf9e289630835107
Milad Nourizade <miladnouriezade@outlook.com>
2018-07-12 13:31:16 +0430

    Initial Commit

---
+++ DigiStore/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DigiStore
+//
+//  Created by Milad Nourizade on 7/12/18.
+//  Copyright © 2018 Milad Nourizade. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
